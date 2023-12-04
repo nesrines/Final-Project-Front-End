5f13ed0f41fa9074362abe573d069e1479a1c928
nesrines <nasrinfa@code.edu.az>
2023-12-04 16:35:05 +0400

    footer done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    if (location.pathname == "/") {
+    if (location.pathname == "/" || location.pathname == "/index.html") {
         $('#cat_list').removeClass('hide');
     }
 });
