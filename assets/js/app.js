f739b106e2a82ee0da77fc7f88bd5ba5bd17f327
nesrines <nasrinfa@code.edu.az>
2023-12-12 23:06:49 +0400

    details page half done

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,4 +4,25 @@ $(document).ready(function() {
         location.pathname == '/shop.html') {
         $('#cat_list').removeClass('hide');
     }
+
+  $('.zoom').mousemove(function(e) {
+    const width = $(this).width();
+    const height = $(this).height();
+
+    const mouseX = e.pageX - $(this).offset().left;
+    const mouseY = e.pageY - $(this).offset().top;
+    
+    const moveX = -(mouseX / width - 0.5) * 1.7 * 100;
+    const moveY = -(mouseY / height - 0.5) * 1.7 * 100;
+
+    $('.zoom img').css({
+        'transform': `scale(1.7) translate(${moveX}%, ${moveY}%)`,
+        'transform-origin': 'center center',
+    });
+  });
+
+  $('.zoom').mouseleave(function() {
+    $('.zoom img').css({'transform': 'scale(1)'});
+  });
+ 
 });
