11597ca559bd17c375bcdcbe9aca3681fc3ef679
nesrines <nasrinfa@code.edu.az>
2023-12-14 00:12:20 +0400

    wishlist and basket

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,24 +5,24 @@ $(document).ready(function() {
         $('#cat_list').removeClass('hide');
     }
 
-  $('.zoom').mousemove(function(e) {
+    $('.zoom').mousemove(function(e) {
     const width = $(this).width();
     const height = $(this).height();
 
     const mouseX = e.pageX - $(this).offset().left;
     const mouseY = e.pageY - $(this).offset().top;
     
-    const moveX = -(mouseX / width - 0.5) * 1.7 * 100;
-    const moveY = -(mouseY / height - 0.5) * 1.7 * 100;
+    const moveX = -(mouseX / width - 0.5) * 1.8 * 100;
+    const moveY = -(mouseY / height - 0.5) * 1.8 * 100;
 
-    $('.zoom img').css({
-        'transform': `scale(1.7) translate(${moveX}%, ${moveY}%)`,
-        'transform-origin': 'center center',
+        $('.zoom img').css({
+            'transform': `scale(1.8) translate(${moveX}%, ${moveY}%)`,
+            'transform-origin': 'center center',
+        });
     });
-  });
 
-  $('.zoom').mouseleave(function() {
-    $('.zoom img').css({'transform': 'scale(1)'});
-  });
+    $('.zoom').mouseleave(function() {
+      $('.zoom img').css({'transform': 'scale(1)'});
+    });
  
 });
